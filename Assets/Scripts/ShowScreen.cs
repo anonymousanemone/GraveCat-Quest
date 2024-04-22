7d6d83db12b58172a3eb08da97c1e05c85bf584f
Joanna Nieh <jan2181@barnard.edu>
2024-04-22 16:17:34 -0400

    start screen functional

---
+++ Assets/Scripts/ShowScreen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowScreen : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(true);
+
+    }
+}
