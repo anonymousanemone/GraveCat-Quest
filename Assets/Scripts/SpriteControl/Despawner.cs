ea680709b32d40969dca22da3841fc2b7246642e
Anastasija Tortevska <anastasija.tortevska@columbia.edu>
2024-04-27 14:01:09 -0400

    boss music + bird despawn

---
+++ Assets/Scripts/SpriteControl/Despawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawner : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+            Destroy(other.gameObject);
+    }
+}
