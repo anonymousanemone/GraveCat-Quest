115d0d07ed8ee1d538f79ea270a808b149ed3813
Sophia Ling <sosophia0702@gmail.com>
2024-05-09 15:32:20 -0400

    saving minor changes

--- Assets/Scripts/GameManagers/Continue.cs
+++ Assets/Scripts/GameManagers/Continue.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Continue : MonoBehaviour
 {
     private int sceneToContinue;
+    [SerializeField] TextMeshProUGUI buttonText;
+    private void Awake()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+    }
 
+    private void Update()
+    {
+
+        if (sceneToContinue != 0)
+        {
+            buttonText.text = "Resume";
+        }
+    }
     public void ContinueGame(){
         sceneToContinue = PlayerPrefs.GetInt("SavedScene");
 
